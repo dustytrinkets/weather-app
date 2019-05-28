622d1714e239ae1dcfed13233ea4fff4dccd138d
Laura Corbi Villalba <lcorbivi@everis.com>
2019-05-28 10:42:29 +0200

    type validation for data

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
+import PropTypes from 'prop-types';
+import * as constWeather from '../../../constants/weathers'
+
 import './styles.css'
 
-const WeatherData = ({data}) => {
-    const {temperature, state, humidity, wind} = data;
+const WeatherData = ({data: {temperature, state, humidity, wind}}) => {
+    // const {temperature, state, humidity, wind} = data;
     return (
     <div className="weatherDataCont">
         <WeatherTemperature temperature={temperature} state={state} />
@@ -12,5 +15,16 @@ const WeatherData = ({data}) => {
     </div>
 
 )}
+
+
+WeatherData.propTypes = {
+    data: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        state: PropTypes.oneOf(Object.values(constWeather)).isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind:PropTypes.string.isRequired
+    }),
+}
+
 
 export default WeatherData;
